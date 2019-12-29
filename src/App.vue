aadb29cc810a6d1eb3b69cac79865f52811eb927
DreamZoom <1036199213@qq.com>
2019-12-29 21:49:06 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,61 @@
 <template>
-  <ying-app :config="config">
-  </ying-app>
+  <ying-app :config="config"></ying-app>
 </template>
 
 <script>
-  import table from '@/views/table';
-  import layout from '@/views/layout';
-  export default {
-    name: 'App',
-    methods: {
-      config(context) {
+import table from "@/views/table";
+import layout from "@/views/layout";
+export default {
+  name: "App",
+  methods: {
+    config(context) {
+      context.addRoute("user", table);
 
-        context.addRoute("user",table);
-       
-        
-        context.addMenu('main', 'file', '文件', 'el-icon-eleme');
-        context.addMenu('main', 'edit', '编辑', 'el-icon-eleme');
-        context.addMenu('main', 'view', '视图', 'el-icon-eleme');
-        context.addMenu('tools', 'message', '消息', 'el-icon-eleme');
-        context.addMenu('slide', 'wjxt', '问卷系统', 'el-icon-eleme');
-        context.addMenu('wjxt', 'wjgl', '问卷管理', 'el-icon-eleme');
-        context.addMenu('slide', 'wjxt1', '问卷系统', 'el-icon-eleme',()=>{
-          context.push("/user")
-        });
-    
-      }
+      context.config_menu("main", [
+        {
+          name: "file",
+          title: "文件",
+          icon: "el-icon-eleme",
+          action: () => {
+            console.log("文件");
+          }
+        },
+        {
+          name: "edit",
+          title: "编辑",
+          icon: "el-icon-eleme",
+          action: () => {
+            console.log("编辑");
+          }
+        },
+        {
+          name: "view",
+          title: "视图",
+          icon: "el-icon-eleme",
+          action: () => {
+            console.log("视图");
+          }
+        }
+      ]);
+
+      context.config_menu("slide", [
+        {
+          name: "wjxt",
+          title: "问卷系统",
+          icon: "el-icon-eleme",
+          action: () => {
+            context.push("/user");
+          }
+        }
+      ]);
     }
   }
+};
 </script>
 
 <style>
-  #app {
-    width: 100%;
-    height: 100%;
-  }
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/components/app/store.js
+++ src/components/app/store.js
@@ -3,25 +3,15 @@ import Vuex from 'vuex';
 import http from '../http';
 Vue.use(Vuex);
 
-const findMenu = (menus, name) => {
-	for (var i = 0; i < menus.length; i++) {
-		var menu = menus[i];
-		if (menu == null) continue;
-		if (menu.name == name) {
-			return menu;
-		}
-		if(menu.childs){
-			var cmenu = findMenu(menu.childs, name);
-			if (cmenu != null) return cmenu;
-		}	
-	}
-	return null;
-};
-let combMenu = (menus,mode) => {
-	mode=mode||"horizontal";
+let find_menu = (menus,key)=>{
+	
+}
+
+let make_menu = (menus, mode) => {
+	mode = mode || "horizontal";
 	var template = menus.map((item) => {
 		if (item.childs) {
-			var child_template = combMenu(item.childs);
+			var child_template = make_menu(item.childs);
 			return `<el-submenu index="${item.name}"><template slot="title"><i class="${item.icon}"></i><span>${item.title}</span></template> ${child_template}</el-submenu>`;
 		} else {
 			return `<el-menu-item index="${item.name}"> <i class="${item.icon}"></i><span>${item.title}</span></el-menu-item>`;
@@ -31,10 +21,6 @@ let combMenu = (menus,mode) => {
 };
 
 const state = {
-	count: 1,
-	client_id: '',
-	client_secret: '',
-
 	user: {
 		authenticated: false,
 		token: window.localStorage.getItem('token'),
@@ -44,22 +30,15 @@ const state = {
 		}
 	},
 	app: {
-		menus: [
-			{
-				name: 'main',
-				childs: []
-			},
-			{
-				name: 'slide',
-				childs: []
-			},
-			{
-				name: 'tools',
-				childs: []
-			}
-		],
-		routes:[],
-		hideSide:false
+		client_id: '',
+		client_secret: '',
+		menus:{
+			'main':[],
+			'slide':[],
+			'tools':[]
+		},
+		routes: [],
+		slide: false
 	}
 };
 
@@ -68,64 +47,52 @@ const getters = {
 		return state.user.authenticated ? state.user.info.nickname : '未登录';
 	},
 	main_menus(state) {
-		var menus = findMenu(state.app.menus, 'main').childs;
-		var template = combMenu(menus);
+		var menus = state.app.menus.main;
+		var template = make_menu(menus);
 		template = `<el-menu @select="handleSelect" mode="horizontal">${template}</el-menu>`;
-		return { template,methods:{handleSelect(index,indexPath){this.$store.commit('tiggerMenu',{index,indexPath})}} };
+		return { template, methods: { handleSelect(index, indexPath) { this.$store.commit('tiggerMenu', { key:"main",index, indexPath }) } } };
 	},
 	slide_menus(state) {
-		var menus = findMenu(state.app.menus, 'slide').childs;
-		var template = combMenu(menus);
+		var menus = state.app.menus.slide;
+		var template = make_menu(menus);
 		template = `<el-menu @select="handleSelect" mode="vertical" :collapse="$store.getters.side" :collapse-transition="false" background-color="#20222A" text-color="#fff" active-text-color="#ffd04b">${template}</el-menu>`;
-		return { template ,methods:{handleSelect(index,indexPath){this.$store.commit('tiggerMenu',{index,indexPath})}}};
+		return { template, methods: { handleSelect(index, indexPath) { this.$store.commit('tiggerMenu', { key:"slide",index, indexPath }) } } };
 	},
 	tools_menus(state) {
-		var menus = findMenu(state.app.menus, 'tools').childs;
-		var template = combMenu(menus);
+		var menus = state.app.menus.tools;
+		var template = make_menu(menus);
 		template = `<el-menu @select="handleSelect" mode="horizontal">${template}<el-submenu index="2"><div slot="title">{{$store.getters.username}}</div><el-menu-item index="2-1">修改密码</el-menu-item><el-menu-item index="2-2">注销登录</el-menu-item></el-submenu></el-menu>`;
-		return { template,methods:{handleSelect(index,indexPath){this.$store.commit('tiggerMenu',{index,indexPath})}} };
+		return { template, methods: { handleSelect(index, indexPath) { this.$store.commit('tiggerMenu', { key:"tools",index, indexPath }) } } };
 	},
-	routes(state){
+	routes(state) {
 		return state.app.routes;
 	},
-	side(state){
-		console.log(state.app.hideSide);
-		return state.app.hideSide;
+	slide(state) {
+		return state.app.slide;
 	}
 };
 
 const mutations = {
-	addMenu(state, menu) {
-		const parent = menu.parent;
-		console.log(parent);
-		var p = findMenu(state.app.menus, parent);
-		if (p) {
-			if (!p.childs) {
-				console.warn(`${parent} has not childs attribute,must set it`);
-				p.childs = [];
-			}
-			p.childs.push({ ...menu });
+	menu(state, menu){
+		var k = menu.key;
+		state.app.menus[k]=menu.menus;
+	},
+	tiggerMenu(state, options) {
+		var menu = find_menu(state.app.menus[options.key], options.index);
+		if (menu && menu.action && typeof menu.action =='function') {
+			menu.action(menu);
 		}
 	},
-	tiggerMenu(state,menu){
-		console.log(menu);
-		var p = findMenu(state.app.menus, menu.index);
-		if(p && p.action){
-			p.action(p);
+	toggleSide(state, show) {
+		if (show != undefined) {
+			state.app.slide = show;
+		}
+		else {
+			state.app.slide = !state.app.slide;
 		}
 	},
-	toggleSide(state,show){
-		
-		if(show!=undefined){
-			state.app.hideSide = show;
-		}
-		else{
-			state.app.hideSide=!state.app.hideSide;
-		}
-		
-	},
-	addRoute(state, route){
-		state.app.routes.push({...route});
+	addRoute(state, route) {
+		state.app.routes.push({ ...route });
 	},
 	setClient(state, client_id, client_secret) {
 		state.client_id = client_id;
